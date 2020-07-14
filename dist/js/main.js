0b3eddc3c33ddcfadcbd5bf85650ca8e7ea11462
francois.rice <francois.rice@oddnetworks.com>
2020-07-14 03:30:11 -0400

    Make sidenav mobile friendly & responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 // function getDOMItems()
 const menuBtn = document.querySelector('.menu-btn');
 // const menu = document.querySelector('.menu');
+const btnLines = document.querySelectorAll('.btn-line');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
@@ -41,6 +42,7 @@ function toggleMenu(params) {
 	if (!showMenu) {
 		menuBtn.classList.add('close');
 		// menu.classList.add('show');
+		btnLines.forEach((item) => item.classList.add('show'));
 		menuNav.classList.add('show');
 		menuBranding.classList.add('show');
 		navItems.forEach((item) => item.classList.add('show'));
@@ -51,6 +53,7 @@ function toggleMenu(params) {
 	} else {
 		menuBtn.classList.remove('close');
 		// menu.classList.remove('show');
+		btnLines.forEach((item) => item.classList.remove('show'));
 		menuNav.classList.remove('show');
 		menuBranding.classList.remove('show');
 		navItems.forEach((item) => item.classList.remove('show'));
