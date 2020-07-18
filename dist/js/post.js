c17b899a2ecbcf519ed7cd122da802db95960048
francois.rice <francois.rice@oddnetworks.com>
2020-07-18 06:42:19 -0400

    Create post.html & format footer

---
+++ dist/js/post.js
@@ -0,0 +1,19 @@
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.documentElement.scrollTop < 1) {
+		// document.querySelector('.navbar').style.height = '6%';
+		document.querySelector('.navbar').style.top = '0';
+		document.querySelector('.post').style.marginTop = '7%';
+		document.querySelector('.menu-btn').style.visibility = 'hidden';
+	} else {
+		// document.querySelector('.navbar').style.height = '0%';
+		document.querySelector('.navbar').style.top = '-7%';
+		document.querySelector('.post').style.marginTop = '3%';
+		document.querySelector('.menu-btn').style.visibility = 'visible';
+	}
+}
+
+//document.body.scrollTop < 20 ||
